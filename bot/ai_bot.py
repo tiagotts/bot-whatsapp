e33f2c3d6ea19b5b86044f64321fafc5687c71de
Tiago Stangarlin <tiago.tts@gmail.com>
2025-06-24 09:29:45 -0300

    implementacao com rag e chromadb

--- bot/ai_bot.py
+++ bot/ai_bot.py
@@ -2,9 +2,12 @@ import os
 
 from decouple import config
 
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
@@ -13,20 +16,63 @@ os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
 class AIBot:
 
     def __init__(self):
-        self.__chat = ChatGroq(model='llama-3.1-70b-versatile')
+        self.__chat = ChatGroq(model='llama-3.21-90b-versatile-preview')
+        self.__retriever = self.__build_retriever()
 
-    def invoke(self, question):
-        prompt = PromptTemplate(
-            input_variables=['texto'],
-            template='''
-            Você é um tradutor de textos que traduz o texto do usuário para Italiano.
-            <texto>
-            {texto}
-            </texto>
-            '''
+    def __build_retriever(self):
+        persist_directory = '/app/chroma_data'
+        embedding = HuggingFaceEmbeddings()
+
+        vector_store = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=embedding,
         )
-        chain = prompt | self.__chat | StrOutputParser()
-        response = chain.invoke({
-            'texto': question,
-        })
+        return vector_store.as_retriever(
+            search_kwargs={'k': 30},
+        )
+
+    def __build_messages(self, history_messages, question):
+        messages = []
+        for message in history_messages:
+            message_class = HumanMessage if message.get('fromMe') else AIMessage
+            messages.append(message_class(content=message.get('body')))
+        messages.append(HumanMessage(content=question))
+        return messages
+
+    def invoke(self, history_messages, question):
+        SYSTEM_TEMPLATE = '''
+        Responda as perguntas dos usuários com base no contexto abaixo.
+        Você é um assistente especializado em tirar dúvidas sobre a empresa RTrainers.
+        A empresa RTrainers é uma empresa de studio para cuidar da saúde e bem estar de seus clientes.
+        A empresa oferece serviço personalizado para cada cliente, em um ambiente fechado.
+        Sua aula é marcada previamente e o cliente tem um personal trainer para cuidar dele.
+        O personal trainer é um profissional de saúde e bem estar que cuida da saúde e bem estar de seus clientes.
+        Tire dúvidas dos possíveis alunos que entrarem em contato.
+        Responda de forma natural, agradável e respeitosa. Seja objetivo nas respostas, com informações
+        claras e diretas. Foque em ser natural e humanizado, como um diálogo comum entre duas pessoas.
+        Leve em consideração também o histórico de mensagens da conversa com o usuário.
+        Responda sempre em português brasileiro.
+
+        <context>
+        {context}
+        </context>
+        '''
+
+        docs = self.__retriever.invoke(question)
+        question_answering_prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    'system',
+                    SYSTEM_TEMPLATE,
+                ),
+                MessagesPlaceholder(variable_name='messages'),
+            ]
+        )
+        document_chain = create_stuff_documents_chain(self.__chat, question_answering_prompt)
+        response = document_chain.invoke(
+            {
+                'context': docs,
+                'messages': self.__build_messages(history_messages, question),
+            }
+        )
         return response
---
+++ rag/rag.py
@@ -0,0 +1,34 @@
+import os
+
+from decouple import config
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+os.environ['HUGGINGFACE_API_KEY'] = config('HUGGINGFACE_API_KEY')
+
+if __name__ == '__main__':
+    file_path = '/app/rag/data/DocumentoInicial.pdf'
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    chunks = text_splitter.split_documents(
+        documents=docs,
+    )
+
+    persist_directory = '/app/chroma_data'
+
+    embedding = HuggingFaceEmbeddings()
+    vector_store = Chroma(
+        embedding_function=embedding,
+        persist_directory=persist_directory,
+    )
+    vector_store.add_documents(
+        documents=chunks,
+    )
